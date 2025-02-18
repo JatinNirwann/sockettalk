94b1a94b1286f45fc0bfb7232e15b9b2c5d9ee8d
JatinNirwann <141300023+JatinNirwann@users.noreply.github.com>
2025-02-18 13:36:18 +0530

    multiple clients support

--- client.py
+++ client.py
@@ -8,7 +8,7 @@ encoder = "ascii"
 bytesize = 1024
 
 server_ip = input("Enter the IP of server you wish to connect to : ")
-server_port = input("Enter server port number")
+server_port = input("Enter server port number : ")
 displayname = input("Enter your Display name : ")
 
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -32,7 +32,8 @@ def send():
             message =input("Type Below : \n")
             client_socket.send(message.encode(encoder))
 
-while True:
+def main():
+    
     receive_thread = threading.Thread(target=receive)
     receive_thread.start()
     send_thread = threading.Thread(target=send)
--- server.py
+++ server.py
@@ -10,6 +10,27 @@ encoder = "ascii"
 bytesize = 1024
 
 clients = []
+nicknames =[]
+
+def broadcast():
+    for clients in clients:
+        client_socket.send(message.encode(encoder))
+
+def handle_clients(client_socket,client_address):
+    while True:
+        try:
+            message= client_socket.recv(bytesize).decode(encoder)
+            broadcast(message)
+        
+        except:
+            index = clients.index(client_socket)
+            clients.remove(client_socket)
+            client_socket.close()
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+            broadcast(f"{nickname} left ..")
+            break
+
 
 
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -20,7 +41,9 @@ server_socket.listen()
 print(f"Server is listening on {server_ip}:{server_port}....\n")
 
 client_socket , client_address = server_socket.accept()
-
+client_socket.send("NICKNAME".encode(encoder))
+nickname = client_socket.recv(bytesize).decode(encoder)
+print(f"Client {nickname} connected from {client_address}")
 client_socket.send(f"You are now connected to server ({server_ip})\n".encode(encoder))
 client_socket.send("NICKNAME".encode(encoder))
 
