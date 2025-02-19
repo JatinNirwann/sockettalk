5dc8921e63c3d6db26660830e99c674a94b856d1
JatinNirwann <141300023+JatinNirwann@users.noreply.github.com>
2025-02-19 11:13:17 +0530

    Update stun_check.py

--- stun_check.py
+++ stun_check.py
@@ -1,25 +1,9 @@
-#to import this run "pip install pystun3"
 import stun
 
-stun_servers = [
-    "stun.l.google.com", 
-    "stun1.l.google.com",
-    "stun2.l.google.com",
-    "stun3.l.google.com",
-    "stun4.l.google.com",
-    "stun.ekiga.net",
-    "stun.ideasip.com",
-    "stun.sipgate.net",
-    "stun.voipstunt.com"
-]
+stun_host = "stun.l.google.com"
+stun_port = 19302  # Google's STUN Server
 
-for server in stun_servers:
-    try:
-        print(f"Trying STUN server: {server}")
-        nat_type, external_ip, external_port = stun.get_ip_info(stun_host=server)
-        if external_ip:
-            print(f"NAT Type: {nat_type}")
-            print(f"Your Public IP: {external_ip}, Port: {external_port}")
-            break
-    except Exception as e:
-        print(f"Failed with {server}: {e}")
+nat_type, external_ip, external_port = stun.get_ip_info(stun_host=stun_host, stun_port=stun_port)
+
+print(f"NAT Type: {nat_type}")
+print(f"Your Public IP: {external_ip}, Port: {external_port}")
