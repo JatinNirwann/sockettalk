81ab79efb2fb386c92ef6121c945ce91d8c4f961
Jatin <rexifiedjatin@gmail.com>
2025-02-20 23:36:42 +0530

    changed hardcoded ip to dynamic

--- client.py
+++ client.py
@@ -10,13 +10,29 @@ bytesize = 1024
 
 #static server ip and ports , only uncomment these when you know what you doing
 
-server_ip="2401:xxxx:xxxx:xxxx:xxxx:xxxx:xxxx:xxxx"
-server_port = 9090
-
 # server_ip = input("Enter the IP of server you wish to connect to : ")
 # server_port = input("Enter server port number : ")
+
+#uncomment this one when hardcodding ipv6
+# server_ip="2401:xxxx:xxxx:xxxx:xxxx:xxxx:xxxx:xxxx" 
+server_port = 9090
+
 displayname = input("Enter your Display name : ")
 
+def get_global_ipv6():
+    hostname = socket.gethostname()
+    addresses = socket.getaddrinfo(hostname, None, socket.AF_INET6)
+
+    for addr in addresses:
+        ip = addr[4][0]
+        if not ip.startswith("fe80") and not ip.startswith("::1") and not ip.startswith("fd"):
+            return ip  # found global ipv6 address
+    return None
+ipv6 = get_global_ipv6()
+if ipv6:
+    server_ip = ipv6
+else:
+    print("Can not find global IPV6 addr")
 client_socket = socket.socket(socket.AF_INET6,socket.SOCK_STREAM)
 client_socket.connect((server_ip,int(server_port)))
 
